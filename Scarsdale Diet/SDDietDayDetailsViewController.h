3f2ceeeea8c0d9eca2acacf0f170a81187a807f0
Mihail Velikov <mihailvelik@gmail.com>
2014-01-03 14:54:29 +0200

    implement UITableViewController methods and UITableViewDelegate protocol methods

--- Scarsdale Diet/SDDietDayDetailsViewController.h
+++ Scarsdale Diet/SDDietDayDetailsViewController.h
@@ -9,14 +9,17 @@
 #import <UIKit/UIKit.h>
 
 
-@interface SDDietDayDetailsViewController : UIViewController <UISplitViewControllerDelegate>
-- (void)configureView;
+@interface SDDietDayDetailsViewController : UITableViewController <UITableViewDelegate, UISplitViewControllerDelegate>
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+//- (void)configureView;
 @property (strong, nonatomic) id detailItem;
 @property (weak, nonatomic) IBOutlet UIImageView *detailImage;
-@property (weak, nonatomic) IBOutlet UITextView *lunchTextView;
-@property (weak, nonatomic) IBOutlet UITextView *dinnerTextView;
-@property (weak, nonatomic) IBOutlet UITextView *breakfastTextView;
-@property (weak, nonatomic) IBOutlet UITextView *replaceMealTextView;
+
+//@property (weak, nonatomic) IBOutlet UIImageView *detailImage;
+//@property (weak, nonatomic) IBOutlet UITextView *lunchTextView;
+//@property (weak, nonatomic) IBOutlet UITextView *dinnerTextView;
+//@property (weak, nonatomic) IBOutlet UITextView *breakfastTextView;
+//@property (weak, nonatomic) IBOutlet UITextView *replaceMealTextView;
 - (IBAction)shareButton:(id)sender;
 
 
